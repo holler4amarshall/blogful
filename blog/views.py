e8e9f54e87d265556e7517423e525440bc966b79
holly <holler4amarshall@hotmail.com>
2016-10-14 01:24:04 +0000

    fix entries per page drop down feature

--- blog/views.py
+++ blog/views.py
@@ -6,40 +6,35 @@ from flask import request, redirect, url_for
 PAGINATE_BY = 10
 
 @app.route("/")
-@app.route("/", methods=["POST"])
-def entries_per_page(): 
-    PAGINATE_BY = request.args.get('entries_per_page')
-#    entries_per_page = (request.form.get['entries_per_page'])
-    #print (str(PAGINATE_BY))
-    return str(PAGINATE_BY)
-
 @app.route("/page/<int:page>")
 def entries(page=1):
-    
-    #PAGINATE_BY = entries_per_page
+    limit = 100
     
     # Zero-indexed page
     page_index = page - 1
+    
+    paginate_by = int(request.args.get('entries_per_page', PAGINATE_BY))
 
     count = session.query(Entry).count()
 
-    start = page_index * PAGINATE_BY
-    end = start + PAGINATE_BY
+    start = page_index * paginate_by
+    end = start + paginate_by
 
-    total_pages = (count - 1) // PAGINATE_BY + 1
+    total_pages = (count - 1) // paginate_by + 1
     has_next = page_index < total_pages - 1
     has_prev = page_index > 0
 
     entries = session.query(Entry)
     entries = entries.order_by(Entry.datetime.desc())
     entries = entries[start:end]
-
+    
     return render_template("entries.html",
         entries=entries,
         has_next=has_next,
         has_prev=has_prev,
         page=page,
         total_pages=total_pages,
+        limit=limit,
     )
     
 @app.route("/entry/add", methods=["GET"])
