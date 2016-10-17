7c5874a90d2d2d883d36711dd9c042ede9c1518a
holly <holler4amarshall@hotmail.com>
2016-10-17 03:10:27 +0000

    add method to POST login information

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from . import app
-from .database import session, Entry
-from flask import request, redirect, url_for
+from .database import session, Entry, User
+from flask import request, redirect, url_for, flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
 
 PAGINATE_BY = 10
 
@@ -103,5 +105,17 @@ def cancel_delete():
 @app.route("/login", methods=["GET"])
 def login_get():
     return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
     
     
