ab666e87f9505426a903e1bbab3c00ac09fb581c
holly <holler4amarshall@hotmail.com>
2016-11-07 02:23:07 +0000

    update splinter/phantomjs acceptance tests to cover more scenarios

--- tests/test_views_acceptance.py
+++ tests/test_views_acceptance.py
@@ -33,6 +33,9 @@ class TestViews(unittest.TestCase):
         self.process.start()
         time.sleep(1)
         
+    def verify_element_exists(self, xpath):
+        self.assertTrue(self.browser.is_element_present_by_xpath(xpath), "path {} should exist".format(xpath))
+    
     def test_login_correct(self):
         #self.browser.visit("http://127.0.0.1:8080/login")
         self.browser.visit("http://127.0.0.1:8080/login")
@@ -41,9 +44,6 @@ class TestViews(unittest.TestCase):
         button = self.browser.find_by_css("button[type=submit]")
         button.click()
         self.assertEqual(self.browser.url, "http://127.0.0.1:8080/")
-        print(self.browser.is_text_present('Login'))
-        print(self.browser.is_text_present('Logout', wait_time=2))
-        print(self.browser.is_text_not_present('Logout'))
         
     def test_login_incorrect(self):
         self.browser.visit("http://127.0.0.1:8080/login")
@@ -52,21 +52,35 @@ class TestViews(unittest.TestCase):
         button = self.browser.find_by_css("button[type=submit]")
         button.click()
         self.assertEqual(self.browser.url, "http://127.0.0.1:8080/login")
-        self.browser.is_text_present('Login')
-        self.browser.is_text_not_present('Logout')
+        
+    def test_add_entry(self): 
+        title = "a super cool test title 123"
+        content = "even cooler content !!!"
+        self.test_login_correct()
+        self.browser.visit("http://127.0.0.1:8080/entry/add")
+        self.browser.is_element_present_by_xpath('//h1[contains(text(),"Add Entry")]')
+        self.browser.is_element_present_by_xpath('//label[text()="Title"]')
+        self.browser.is_element_present_by_xpath('//label[text()="Content"]')
+        self.browser.fill("title", title)
+        self.browser.fill("content", content)
+        button = self.browser.find_by_css("button[type=submit]")
+        button.click()
+        self.assertEqual(self.browser.url, "http://127.0.0.1:8080/")
+        self.verify_element_exists('//a[text()="a super cool test title 123"]')
+        self.browser.is_element_present_by_xpath('//a[text()="{}"]'.format(title))
+        print(self.browser.is_element_present_by_xpath('//p[text()="{}"]'.format(content)))
+        print(self.browser.is_element_present_by_xpath('//p[text()="created by: {0}"]'.format(self.user.name)))
+        
         
     def test_logout(self):
         self.test_login_correct()
         print('Logged in')
-        logout_link = self.browser.find_link_by_text('Logout')
-        print(self.browser.is_text_present('Logout'))
-        print('Logout link found')
-        logout_link.click()
+        self.browser.click_link_by_partial_text('Logout')
         print('Logout link clicked')
         self.assertEqual(self.browser.url, "http://127.0.0.1:8080/")
-        self.browser.is_text_present('Login')
-        self.browser.is_text_not_present('Logout')
-        print('Login link displayed, Logout link not displayed')
+        print(self.browser.is_text_present('Login'))
+        print(self.browser.is_text_not_present('Logout'))
+        print('Login link displayed, Logout link not displayed as per above')
 
 
     def tearDown(self):
